fa94037ce071b0e0163efc8ff843681c670e0cf6
RAvil <russian82@mail.com>
2019-11-23 22:17:33 -0500

    with out prompt of criteria

--- script.js
+++ script.js
@@ -1,14 +1,96 @@
-function CheckPassword(inputtxt) 
-{ 
-var passw=  /^[A-Za-z]\w{7,14}$/;
-if(inputtxt.value.match(passw)) 
-{ 
-alert('Correct, try another...')
-return true;
+// DOM elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(
+     hasLower, 
+     hasUpper, 
+     hasNumber, 
+     hasSymbol, 
+     length
+    );
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+    var generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter
+    (
+        item => Object.values(item) [0]
+        );
+    if(typesCount ===0) {
+        return '';
+    }
+
+    for(var i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type) [0];
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+    console.log(generatedPassword.slice(0,length));
+
+    const finalPassword = generatedPassword.slice(0, length)
+
+    return finalPassword;
 }
-else
-{ 
-alert('Wrong...!')
-return false;
+// copy to clipboard
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if(!password) {
+        return;
+    }
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password copied to clipboard')
+} );
+
+// Genarator fuctions
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+
 }
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+
 }
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}{}=<>/<>';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
