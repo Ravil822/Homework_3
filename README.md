f8704ae7393050c8907172712f8323cfc4302ef8
RAvil <russian82@mail.com>
2020-01-25 23:04:54 -0500

    addaed copy function and readme

--- README.md
+++ README.md
@@ -5,9 +5,20 @@
 I created an application of generate random password based on user-selected criteria. 
 
 
+# Repository access link
+
+https://github.com/Ravil822/Developer_profile_generator
+
+# Deployed app link
+
+https://ravil822.github.io/Developer_profile_generator/
+
+# Demo
+![](assets/password.gif)
+
+
 * Used techology to make the project:
  * HTLM
- ![](photos/Screenshot (19).png)
  * CSS
  * JS
 
--- script.js
+++ script.js
@@ -1,51 +1,70 @@
-/// set varibles for buttons
+// set varibles for buttons
 var generatePasswordBotton = document.getElementById("generate");
 var copyPassword = document.getElementById("clipboard");
+
+// set varible for password result
+var passwordResult = document.getElementById("result");
+
+// varibles for password criteria
 var numbers = "0123456789";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRDTUVWXYZ"
 var symbols = "!@#$%^&*(){}{}=<>/<>"
-var passwordResult = document.getElementById("result");
-
-
-// varibles for password criteria
-var length, useSymbols, useLower, upperLower;
+var length, useSymbols, useLower, useUpper;
   
+// function that check characters length
 function generatePasswordLeght() {
     console.log(length);
     if (length < 8 || length > 128) {
         alert("Must be must be between 8 and 128 characters");
     } else {
-        var newPassword = generatePassword();
+        let newPassword = generatePassword();
         passwordResult.textContent = newPassword;
-        }
-        console.log(newPassword);
+    }   
 }
 
+// function to generate password
 function generatePassword(){
     var availbleCharacters = "";
     availbleCharacters += numbers;
     var password = "";
     if(useSymbols){ 
-        availbleCharacters += symbols
-        password += symbols.charAt(Math.floor(Math.random() * symbols.length))
+        availbleCharacters += symbols;
+        password += symbols.charAt(Math.floor(Math.random() * symbols.length));
     } 
     if(useLower){ 
-        availbleCharacters += lowerCase
-        password += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length))
+        availbleCharacters += lowerCase;
+        password += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
+    }
+    if(useUpper){ 
+        availbleCharacters += upperCase;
+        password += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
     }
     while(password.length < length) {
-        password += availbleCharacters.charAt(Math.floor(Math.random() * availbleCharacters.length))
-
+        password += availbleCharacters.charAt(Math.floor(Math.random() * availbleCharacters.length));
     }
-    return password
+    return password;  
 };
 
+// create event to generate password botton
 generatePasswordBotton.addEventListener("click", function(){
  length = parseInt(prompt("How many characters would you like? Must be must be between 8 and 128 characters"));
 useSymbols = confirm("Would you like to use symbols?");
 useLower = confirm("Would you like to use lowercase letters?");
-upperLower = confirm("Would you like to use uppercase letters?");
+useUpper = confirm("Would you like to use uppercase letters?");
 generatePasswordLeght();
-
 });
+
+// create event to generate copy password botton
+copyPassword.addEventListener("click", function(){
+    var text = passwordResult.textContent;
+    navigator.clipboard.writeText(text).then(function() {
+        alert('Copying to clipboard was successful!');
+      }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+      });
+});
+
+
+
+
